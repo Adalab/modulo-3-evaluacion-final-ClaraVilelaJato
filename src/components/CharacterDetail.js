2d2b97620f864522a3decc7969ad30c277182179
Clara Vilela <claravilelajato@gmail.com>
2021-10-03 22:50:38 +0200

    Add conditional for invalid route

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,19 +1,22 @@
 const CharacterDetail = (props) => {
-  return (
-    <>
-      <img
-        className='card__img'
-        src={props.character.photo}
-        alt={props.character.name}
-      />
-      <ul className='character_card'>
-        <li>Status: {props.character.status}</li>
-        <li>Especie: {props.character.specie}</li>
-        <li>Origen: {props.character.planet}</li>
-        <li>Episodios: {props.character.episodes}</li>
-      </ul>
-    </>
-  );
+  if (props.character === undefined) {
+    return <p>Este personaje no existe! Vete a ver la serie!</p>;
+  } else {
+    return (
+      <>
+        <img
+          className='card__img'
+          src={props.character.photo}
+          alt={props.character.name}
+        />
+        <ul className='character_card'>
+          <li>Status: {props.character.status}</li>
+          <li>Especie: {props.character.specie}</li>
+          <li>Origen: {props.character.planet}</li>
+          <li>Episodios: {props.character.episodes}</li>
+        </ul>
+      </>
+    );
+  }
 };
-
 export default CharacterDetail;
